bf91fe2092a6549b366454a77eec8734c6db9335
GGulberdi <ggulberdiyeva@gmail.com>
2021-01-14 13:45:54 -0600

    add last version of todoList react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TodoList from './components/TodoList';
+
+function App () {
+    return (
+      <div>
+        <h1 style={{color:'greenyellow', fontSize: 50}}> Todo List</h1>
+        <TodoList />
+      </div>
+    );
+}
+
+export default App;
